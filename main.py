500fff1a5f0dfec6699d59a09e4609704ebb364e
K4rlosReyes <carlosreyesml18@gmail.com>
2022-10-02 08:56:42 -0400

    change: New Pytorch model with updated data

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ app = Flask(__name__)
 # os.makedirs(data_dir, exist_ok=True)
 device = torch.device("cpu")
 model = TSModel(1)
-model.load_state_dict(torch.load("./model/modelfull.pt", map_location=device))
+model.load_state_dict(torch.load("./model/modeltest50.pt", map_location=device))
 model.eval()
 
 
